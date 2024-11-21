8c9ac2fdef9acbf8f71c48e5173cec1adbf77d57
LaibaRafiq <laibaayy0610@gmail.com>
2024-11-21 23:04:13 +0500

    ViewPatientdone

--- app/ViewPatients.jsx
+++ app/ViewPatients.jsx
@@ -1,51 +1,82 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Image,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import NavBar from '../components/NavBarPatients';
 
 const ViewPatients = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity style={styles.backButton}>
-          <Icon name="arrow-left" size={20} color="#FFF" />
-        </TouchableOpacity>
-        <Text style={styles.headerText}>Patients List</Text>
-        <View style={styles.headerIcons}>
-          <Icon name="user" size={20} color="#FFF" />
-          <Text style={styles.doctorText}>DOCTOR</Text>
+      <View style={styles.main}>
+      {/* Top Section */}
+      <View style={styles.topSection}>
+        {/* Header */}
+        <View style={styles.header}>
+          <TouchableOpacity style={styles.backButton}>
+            <Icon name="arrow-left" size={20} color="#000" />
+          </TouchableOpacity>
+          <Text style={styles.headerText}>Patients List</Text>
+          <Icon name="user-circle" size={20} color="#000" style={styles.profileIcon} />
+        </View>
+
+        {/* Search Bar */}
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search...."
+            placeholderTextColor="#ADC1D8"
+          />
+          <Icon name="search" size={18} style={styles.searchIcon} />
         </View>
       </View>
-      <View style={styles.searchContainer}>
-        <TextInput style={styles.searchInput} placeholder="Search...." placeholderTextColor="#ADC1D8" />
-        <Icon name="search" size={18} color="#ADC1D8" style={styles.searchIcon} />
-      </View>
-      <ScrollView style={styles.patientList}>
-        {Array.from({ length: 4 }).map((_, index) => (
-          <View key={index} style={styles.patientCard}>
-            <Image
-              source={{ uri: 'https://placeholder.pics/svg/39x38' }}
-              style={styles.patientImage}
-            />
-            <View style={styles.patientInfo}>
-              <Text style={styles.patientName}>Patient Name</Text>
-              <Text style={styles.patientCondition}>heart patient</Text>
+
+      {/* Patients List */}
+      <View style={styles.patientsContainer}>
+        <View style={styles.patientsHeader}>
+          <Text style={styles.allPatientsText}>Patients List</Text>
+          <TouchableOpacity>
+            <Icon name="filter" size={20} color="#000" style={styles.filterIcon} />
+          </TouchableOpacity>
+        </View>
+        <ScrollView>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <View key={index} style={styles.patientCard}>
+              {/* Patient Info Section */}
+              <View style={styles.patientInfo}>
+                <Image
+                  source={{
+                    uri: 'https://images.vexels.com/media/users/3/153835/isolated/preview/172ddc045ae1707a415adeae0543a5f0-patient-colored-stroke-icon.png',
+                  }}
+                  style={styles.patientImage}
+                />
+                <View style={styles.patientDetails}>
+                  <Text style={styles.patientName}>Patient Name</Text>
+                  <Text style={styles.patientCondition}>Heart Patient</Text>
+                  <TouchableOpacity style={styles.detailsButton}>
+                    <Text style={styles.detailsButtonText}>Details</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+
+              {/* Action Icons */}
+              <View style={styles.patientActions}>
+                <Icon name="heart" size={16} color="#F28F8F" style={styles.icon} />
+                <Icon name="phone" size={16} color="#000" style={styles.icon} />
+                <Icon name="envelope" size={16} color="#000" style={styles.icon} />
+              </View>
             </View>
-            <View style={styles.patientActions}>
-              <TouchableOpacity style={styles.detailsButton}>
-                <Text style={styles.detailsButtonText}>Details</Text>
-              </TouchableOpacity>
-              <Icon name="phone" size={16} color="#000" style={styles.icon} />
-              <Icon name="envelope" size={16} color="#000" style={styles.icon} />
-            </View>
-          </View>
-        ))}
-      </ScrollView>
-      <View style={styles.footer}>
-        <Icon name="home" size={24} color="#000" />
-        <Icon name="bell" size={24} color="#000" />
-        <Icon name="plus" size={24} color="#000" />
-        <Icon name="user" size={24} color="#000" />
+          ))}
+        </ScrollView>
       </View>
+      </View>
+      <NavBar />
     </View>
   );
 };
@@ -54,107 +85,153 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
+    // paddingTop: 35,
+  },
+  main:{
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    padding: 20,
+    paddingTop: 60,
+  },
+  topSection: {
+    backgroundColor: '#ADC1D8',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+    margin: -20,
+    marginBottom: 30,
+    height: 150,
+    marginTop: -20,
   },
   header: {
-    backgroundColor: '#ADC1D8',
-    paddingVertical: 20,
-    paddingHorizontal: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
+    marginBottom: 10,
   },
   backButton: {
-    padding: 10,
-  },
-  headerText: {
-    fontSize: 22,
-    fontWeight: '700',
-    color: '#FFF',
-  },
-  headerIcons: {
-    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 12,
+    padding: 5,
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  doctorText: {
-    fontSize: 12,
+  headerText: {
+    fontSize: 18,
     fontWeight: '700',
-    color: '#FFF',
-    marginLeft: 5,
+    color: '#000',
+  },
+  profileIcon: {
+    marginLeft: 10,
   },
   searchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F0F4F8',
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    margin: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    paddingHorizontal: 15,
+    height: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
   },
   searchInput: {
     flex: 1,
-    fontSize: 15,
+    fontSize: 14,
     color: '#ADC1D8',
   },
   searchIcon: {
+    color: '#ADC1D8',
     marginLeft: 10,
   },
-  patientList: {
+  patientsContainer: {
     flex: 1,
-    paddingHorizontal: 20,
+    backgroundColor: '#F9F9F9',
+    borderRadius: 15,
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 4,
+  },
+  patientsHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  allPatientsText: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#000',
+  },
+  filterIcon: {
+    padding: 10,
   },
   patientCard: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#F0F4F8',
-    borderRadius: 10,
-    padding: 10,
+    backgroundColor: '#ADC1D8',
+    borderRadius: 15,
+    padding: 15,
     marginBottom: 10,
   },
-  patientImage: {
-    width: 39,
-    height: 38,
-    borderRadius: 19,
-    marginRight: 10,
-  },
   patientInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
     flex: 1,
   },
+  patientImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  patientDetails: {
+    flex: 1,
+
+  },
+  
   patientName: {
-    fontSize: 13,
-    fontWeight: '600',
+    fontSize: 14,
+    fontWeight: '700',
     color: '#000',
+    marginBottom: 5,
   },
   patientCondition: {
-    fontSize: 8,
+    fontSize: 12,
     fontWeight: '400',
+    color: '#333',
+    marginBottom: 10,
+  },
+  detailsButton: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    width: 60,
+  },
+  detailsButtonText: {
+    fontSize: 10,
+    fontWeight: '500',
     color: '#000',
   },
   patientActions: {
     flexDirection: 'row',
     alignItems: 'center',
   },
-  detailsButton: {
-    backgroundColor: '#FFFFFF',
-    borderRadius: 10,
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    marginRight: 5,
-  },
-  detailsButtonText: {
-    fontSize: 8,
-    fontWeight: '500',
-    color: '#263238',
-  },
   icon: {
-    marginHorizontal: 5,
-  },
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 10,
-    borderTopWidth: 1,
-    borderColor: '#E0E0E0',
+    marginLeft: 10,
   },
 });
 
---
+++ components/NavBarPatients.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const NavBar = () => {
+  return (
+    <View style={styles.navContainer}>
+      <TouchableOpacity style={styles.navIcon}>
+        <Icon name="th-large" size={20} color="#576574" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navIcon}>
+        <Icon name="pie-chart" size={20} color="#576574" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navCenter}>
+        <View style={styles.centerButton}>
+          <Icon name="comment" size={24} color="#576574" /> {/* Updated Icon */}
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navIcon}>
+        <Icon name="bell" size={20} color="#576574" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navIcon}>
+        <Icon name="user" size={20} color="#576574" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#ADC1D8',
+    height: 100,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  navIcon: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: "70%",
+    height: "70%",
+  },
+  navCenter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  centerButton: {
+    backgroundColor: '#FFFFFF',
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    position: 'absolute',
+    top: -25,
+  },
+});
+
+export default NavBar;
